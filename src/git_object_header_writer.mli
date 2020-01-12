50d352d6818d51ff5310f039b6f0db977034bcd3
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2020-01-12 22:56:28 +0000

    Share a bit of code for writing git object headers.

---
+++ src/git_object_header_writer.mli
@@ -0,0 +1,23 @@
+open Core
+
+(** [header_length] returns the length required to write down a git object header for an
+    object of the given type and length. *)
+val header_length : Object_type.t -> object_length:int -> int
+
+(** [write_from_left] writes a git object header for the given object type and length to
+    the specified [Bigstring.t] starting from position [pos].
+
+    The method assumes that the given Bigstring has enough space available to be able to
+    write the entire header.
+
+    The return value corresponds to the length of the header data written. *)
+val write_from_left : Bigstring.t -> pos:int -> Object_type.t -> object_length:int -> int
+
+(** [write_from_right] writes a git object header for the given object type and length to
+    the specified [Bigstring.t] ending on position [pos].
+
+    The method assumes that the given Bigstring has enough space available to be able to
+    write the entire header.
+
+    The return value corresponds to the length of the header data written. *)
+val write_from_right : Bigstring.t -> pos:int -> Object_type.t -> object_length:int -> int
