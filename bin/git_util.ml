6701505e84fa5f94de8bbd56403eb538a2bad8e5
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2020-01-26 18:42:00 +0000

    Rename library from Git_object_store to Git.

--- bin/git_util.ml
+++ bin/git_util.ml
@@ -17,7 +17,7 @@
 
 open Core
 open Async
-open Git_object_store
+module Sha1 = Git.Sha1
 
 let read_git_object_file file =
   let sha1 =
@@ -26,18 +26,18 @@ let read_git_object_file file =
     Option.try_with (fun () -> Sha1.Hex.of_string (part2 ^ part3))
   in
   let create_git_object_reader sha1_validation =
-    Git_object_reader.create
+    Git.Object_reader.create
       ~on_blob_size:(fun (_ : int) -> ())
       ~on_blob_chunk:(fun buf ~pos ~len ->
         Writer.write_bigstring (force Writer.stdout) ~pos ~len buf)
-      ~on_commit:(fun commit -> printf !"%{sexp: Commit.t}\n" commit)
+      ~on_commit:(fun commit -> printf !"%{sexp: Git.Commit.t}\n" commit)
       ~on_tree_line:(fun mode sha1 ~name ->
         printf
           !"%19s    %{Sha1.Hex}    %s\n"
-          (Sexp.to_string_hum ([%sexp_of: File_mode.t] mode))
+          (Sexp.to_string_hum ([%sexp_of: Git.File_mode.t] mode))
           (Sha1.Raw.Volatile.to_hex sha1)
           name)
-      ~on_tag:(fun tag -> printf !"%{sexp: Tag.t}\n" tag)
+      ~on_tag:(fun tag -> printf !"%{sexp: Git.Tag.t}\n" tag)
       ~on_error:(fun error -> Error.raise error)
       sha1_validation
   in
@@ -45,75 +45,75 @@ let read_git_object_file file =
     match sha1 with
     | None ->
       let git_object_reader = create_git_object_reader Do_not_validate_sha1 in
-      Git_object_reader.read_file git_object_reader ~file ()
+      Git.Object_reader.read_file git_object_reader ~file ()
     | Some sha1 ->
       let git_object_reader = create_git_object_reader Validate_sha1 in
-      Git_object_reader.read_file git_object_reader ~file sha1)
+      Git.Object_reader.read_file git_object_reader ~file sha1)
 ;;
 
 let read_git_pack_file pack_file =
   let open Deferred.Or_error.Let_syntax in
-  let%map t = Git_pack_reader.create ~pack_file Validate_sha1 in
-  let items_in_pack = Git_pack_reader.items_in_pack t in
+  let%map t = Git.Pack_reader.create ~pack_file Validate_sha1 in
+  let items_in_pack = Git.Pack_reader.items_in_pack t in
   Core.printf "items in pack: %d\n" items_in_pack;
   Core.printf "    idx | %40s\n" "sha1";
   for index = 0 to items_in_pack - 1 do
     Core.printf
       !"%7d | %{Sha1.Hex}\n"
       index
-      (Sha1.Raw.Volatile.to_hex (Git_pack_reader.sha1 t ~index))
+      (Sha1.Raw.Volatile.to_hex (Git.Pack_reader.sha1 t ~index))
   done;
   for index = 0 to items_in_pack - 1 do
     Core.printf
       !"\n%{Sha1.Hex}\n"
-      (Sha1.Raw.Volatile.to_hex (Git_pack_reader.sha1 t ~index));
+      (Sha1.Raw.Volatile.to_hex (Git.Pack_reader.sha1 t ~index));
     try
-      Git_pack_reader.read_object
+      Git.Pack_reader.read_object
         t
         ~index
         ~on_blob_size:(fun (_ : int) -> ())
         ~on_blob_chunk:(fun buf ~pos ~len ->
           Core.printf "Blob chunk:\n%s\n" (Bigstring.To_string.sub buf ~pos ~len))
-        ~on_commit:(Core.printf !"%{sexp: Commit.t}\n")
+        ~on_commit:(Core.printf !"%{sexp: Git.Commit.t}\n")
         ~on_tree_line:(fun file_mode sha1 ~name ->
           Core.printf
-            !"Tree line: %{sexp: File_mode.t} %{Sha1.Hex} %s\n"
+            !"Tree line: %{sexp: Git.File_mode.t} %{Sha1.Hex} %s\n"
             file_mode
             (Sha1.Raw.Volatile.to_hex sha1)
             name)
-        ~on_tag:(Core.printf !"%{sexp: Tag.t}\n")
+        ~on_tag:(Core.printf !"%{sexp: Git.Tag.t}\n")
     with
     | exn ->
       Core.eprintf
         !"Error parsing %{Sha1.Hex}:\n%{Exn}\n"
-        (Sha1.Raw.Volatile.to_hex (Git_pack_reader.sha1 t ~index))
+        (Sha1.Raw.Volatile.to_hex (Git.Pack_reader.sha1 t ~index))
         exn
   done
 ;;
 
-let index_git_pack_file pack_file = Git_pack_reader.index_pack ~pack_file
+let index_git_pack_file pack_file = Git.Pack_reader.index_pack ~pack_file
 
 let read_sha1_from_store ~object_directory sha1 =
   let open Deferred.Or_error.Let_syntax in
-  let%bind git_unified_reader =
-    Git_unified_reader.create ~object_directory ~max_concurrent_reads:1 Validate_sha1
+  let%bind git_object_store =
+    Git.Object_store.create ~object_directory ~max_concurrent_reads:1 Validate_sha1
   in
   Monitor.try_with_or_error ~extract_exn:true (fun () ->
     let open Deferred.Let_syntax in
-    Git_unified_reader.read_object
-      git_unified_reader
+    Git.Object_store.read_object
+      git_object_store
       sha1
       ~on_blob_size:(fun (_ : int) -> ())
       ~on_blob_chunk:(fun buf ~pos ~len ->
         Writer.write_bigstring (force Writer.stdout) ~pos ~len buf)
-      ~on_commit:(fun commit -> printf !"%{sexp: Commit.t}\n" commit)
+      ~on_commit:(fun commit -> printf !"%{sexp: Git.Commit.t}\n" commit)
       ~on_tree_line:(fun mode sha1 ~name ->
         printf
           !"%19s    %s    %s\n"
-          (Sexp.to_string_hum ([%sexp_of: File_mode.t] mode))
+          (Sexp.to_string_hum ([%sexp_of: Git.File_mode.t] mode))
           (Sexp.to_string_hum ([%sexp_of: Sha1.Hex.t] (Sha1.Raw.Volatile.to_hex sha1)))
           (Sexp.to_string_hum ([%sexp_of: string] name)))
-      ~on_tag:(fun tag -> printf !"%{sexp: Tag.t}\n" tag)
+      ~on_tag:(fun tag -> printf !"%{sexp: Git.Tag.t}\n" tag)
       ~push_back:(fun () ->
         let%bind () = Writer.flushed (force Writer.stdout) in
         return `Ok))
@@ -121,15 +121,15 @@ let read_sha1_from_store ~object_directory sha1 =
 
 let write_commit_from_file ~object_directory file =
   Monitor.try_with_or_error ~extract_exn:true (fun () ->
-    let%bind commit = Reader.load_sexp_exn file [%of_sexp: Commit.t] in
-    let%map sha1 = Git_object_writer.Commit.write' ~object_directory commit in
+    let%bind commit = Reader.load_sexp_exn file [%of_sexp: Git.Commit.t] in
+    let%map sha1 = Git.Object_writer.Commit.write' ~object_directory commit in
     printf !"%{Sha1.Hex}\n" (Sha1.Raw.to_hex sha1))
 ;;
 
 let write_tree_from_file ~object_directory file =
   Monitor.try_with_or_error ~extract_exn:true (fun () ->
-    let tree_writer = Git_object_writer.Tree.create_uninitialised ~object_directory in
-    let%bind () = Git_object_writer.Tree.init_or_reset tree_writer in
+    let tree_writer = Git.Object_writer.Tree.create_uninitialised ~object_directory in
+    let%bind () = Git.Object_writer.Tree.init_or_reset tree_writer in
     let%bind raw_tree_lines = Reader.file_lines file in
     List.iter raw_tree_lines ~f:(fun raw_line ->
       let line = Sexp.scan_sexps (Lexing.from_string raw_line) in
@@ -144,7 +144,7 @@ let write_tree_from_file ~object_directory file =
       match line with
       | [ mode; sha1; name ] ->
         let mode =
-          match [%of_sexp: File_mode.t] mode with
+          match [%of_sexp: Git.File_mode.t] mode with
           | exception exn -> fail ~exn:(Some exn)
           | mode -> mode
         in
@@ -154,13 +154,13 @@ let write_tree_from_file ~object_directory file =
           | sha1 -> sha1
         in
         let name = [%of_sexp: string] name in
-        Git_object_writer.Tree.write_tree_line
+        Git.Object_writer.Tree.write_tree_line
           tree_writer
           mode
           (Sha1.Raw.of_hex sha1)
           ~name
       | _ -> fail ~exn:None);
-    let%map sha1 = Git_object_writer.Tree.finalise tree_writer in
+    let%map sha1 = Git.Object_writer.Tree.finalise tree_writer in
     printf !"%{Sha1.Hex}\n" (Sha1.Raw.to_hex sha1))
 ;;
 
@@ -169,16 +169,16 @@ let write_blob_from_file ~object_directory file =
     let%bind stat = Unix.stat file in
     let length = Int64.to_int_exn stat.size in
     let blob_writer =
-      Git_object_writer.Blob.Known_size.create_uninitialised ~object_directory
+      Git.Object_writer.Blob.Known_size.create_uninitialised ~object_directory
     in
     let%bind () =
       Reader.with_file file ~f:(fun reader ->
         let%bind () =
-          Git_object_writer.Blob.Known_size.init_or_reset blob_writer ~length
+          Git.Object_writer.Blob.Known_size.init_or_reset blob_writer ~length
         in
         match%bind
           Reader.read_one_chunk_at_a_time reader ~handle_chunk:(fun buf ~pos ~len ->
-            Git_object_writer.Blob.Known_size.append_data blob_writer buf ~pos ~len;
+            Git.Object_writer.Blob.Known_size.append_data blob_writer buf ~pos ~len;
             return `Continue)
         with
         | `Eof -> Deferred.unit
@@ -188,7 +188,7 @@ let write_blob_from_file ~object_directory file =
               "bug - unexpected read_one_chunk_at_a_time result"
                 (result : _ Reader.read_one_chunk_at_a_time_result)])
     in
-    let%map sha1 = Git_object_writer.Blob.Known_size.finalise_exn blob_writer in
+    let%map sha1 = Git.Object_writer.Blob.Known_size.finalise_exn blob_writer in
     printf !"%{Sha1.Hex}\n" (Sha1.Raw.to_hex sha1))
 ;;
 
@@ -221,7 +221,7 @@ let read_sha1_from_store_command =
     ~summary:
       "print git object identified by sha1 from either a raw object file or a pack file"
     [%map_open.Command
-      let object_directory = Util.object_directory_param
+      let object_directory = Git.Util.object_directory_param
       and sha1 = anon ("SHA1" %: sexp_conv [%of_sexp: Sha1.Hex.t]) in
       fun () -> read_sha1_from_store ~object_directory sha1]
 ;;
@@ -232,7 +232,7 @@ let write_commit_from_file_command =
       "create a commit given a sexp representation in the same format as output by the \
        read commands"
     [%map_open.Command
-      let object_directory = Util.object_directory_param
+      let object_directory = Git.Util.object_directory_param
       and file = anon ("SEXP-FILE" %: Filename.arg_type) in
       fun () -> write_commit_from_file ~object_directory file]
 ;;
@@ -243,7 +243,7 @@ let write_tree_from_file_command =
       "create a tree given a line-based representation in the same format as output by \
        the read commands"
     [%map_open.Command
-      let object_directory = Util.object_directory_param
+      let object_directory = Git.Util.object_directory_param
       and file = anon ("FILE" %: Filename.arg_type) in
       fun () -> write_tree_from_file ~object_directory file]
 ;;
@@ -252,7 +252,7 @@ let write_blob_from_file_command =
   Command.async_or_error
     ~summary:"create a blob given a file"
     [%map_open.Command
-      let object_directory = Util.object_directory_param
+      let object_directory = Git.Util.object_directory_param
       and file = anon ("FILE" %: Filename.arg_type) in
       fun () -> write_blob_from_file ~object_directory file]
 ;;
