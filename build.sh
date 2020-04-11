e1a7e29647b46bbee10fcd3c9ed08e2d90f1101a
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2020-04-11 17:43:54 +0100

    Start using dune auto-formatting.

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 shopt -s globstar
 
 cd "$(dirname "$0")"
-ocamlformat -i ./**/*.ml ./**/*.mli || true
+dune build @fmt --auto-promote || true
 ocp-indent -c JaneStreet -i ./**/*.ml ./**/*.mli || true
 dune build bin/git_util.exe fuse/git_fuse.exe @runtest @install
