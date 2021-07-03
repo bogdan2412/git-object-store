be1f230c3a0cb046c845a625d4fb068ef6f0b0b7
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2021-07-03 17:11:36 +0100

    build.sh formats files correctly on OS X

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -euo pipefail
-shopt -s globstar
 
 cd "$(dirname "$0")"
 dune build @fmt --auto-promote || true
-ocp-indent -c JaneStreet -i ./**/*.ml ./**/*.mli || true
+find . "(" -name "*.ml" -o -name "*.mli" ")" -exec \
+    ocp-indent -c JaneStreet -i {} + || true
 dune build bin/git_util.exe fuse/git_fuse.exe @runtest @install
