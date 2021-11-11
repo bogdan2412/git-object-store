7843c99f67c6150584e67e64cbbb9873bdb23f4d
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2021-11-11 10:55:54 +0000

    Support reading multi-pack-index files.

---
+++ object_store/multi_pack_index_reader.mli
@@ -0,0 +1,34 @@
+open! Core
+open! Async
+open! Import
+
+type t
+
+val open_existing : pack_directory:string -> t Or_error.t Deferred.t
+
+(** Returns the list of pack file names indexed by the multi pack index. *)
+val pack_file_names : t -> string array
+
+(** Returns the number of objects indexed by the multi pack index. *)
+val object_count : t -> int
+
+(** Returns the sha1 corresponding to the object with the provided index position. *)
+val sha1 : t -> index:int -> Sha1.Raw.Volatile.t
+
+(** Returns the index of the pack in [pack_file_names] to which the object with the
+    provided index position belongs. *)
+val pack_id : t -> index:int -> int
+
+(** Returns the offset within its corresponding pack at which the object with the
+    provided index position can be found. *)
+val pack_offset : t -> index:int -> int
+
+(** Search for an object with the given SHA1 hash in the multi pack index. *)
+val find_sha1_index : t -> Sha1.Raw.t -> Git_pack_files.Find_result.Volatile.t
+
+(** Search for an object with the given SHA1 hash in the multi pack index. *)
+val find_sha1_index' : t -> Sha1.Raw.Volatile.t -> Git_pack_files.Find_result.Volatile.t
+
+module For_testing : sig
+  val print_out_multi_pack_index : pack_directory:string -> unit Deferred.t
+end
