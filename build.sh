fc05c9171b78efc6b134fba332e5cfd37e1159d1
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2022-04-03 16:01:51 +0100

    Use [clang-format] for C bindings.

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ cd "$(dirname "$0")"
 dune build @fmt --auto-promote || true
 find . "(" -name "*.ml" -o -name "*.mli" ")" -exec \
     ocp-indent -c JaneStreet -i {} + || true
+find . "(" -name "*.c" -o -name "*.h" ")" -exec \
+    clang-format -i {} + || true
 dune build bin/git_util.exe fuse/git_fuse.exe @runtest @install
