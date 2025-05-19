cf82034cd72d020bda78dc3e79db5d6d11a8f00a
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2025-05-19 19:21:17 +0100

    Update copyright headers for 2025.

--- bindings/zlib/zlib_stubs.c
+++ bindings/zlib/zlib_stubs.c
@@ -1,6 +1,6 @@
 /* zlib bindings for OCaml.
 
-   Copyright (C) 2019-2024  Bogdan-Cristian Tataroiu
+   Copyright (C) 2019-2025  Bogdan-Cristian Tataroiu
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
--- pack_files/index_writer.ml
+++ pack_files/index_writer.ml
@@ -1,6 +1,6 @@
 (** Library for manipulating a git object store via OCaml.
 
-    Copyright (C) 2021-2024  Bogdan-Cristian Tataroiu
+    Copyright (C) 2021-2025  Bogdan-Cristian Tataroiu
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
